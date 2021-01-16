f46bcdf94e0994ff09774ab3a1da1bc3463ef1df
Piotr Gemziak <piotrgemziak@gmail.com>
2021-01-16 20:32:26 +0100

    test 2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use std::error;
+
 use netlify_lambda_http::{
     handler,
+    http::Method,
     lambda::{self, lambda, Context},
     IntoResponse, Request, RequestExt,
 };
 
-type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+type Error = Box<dyn error::Error + Send + Sync + 'static>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -13,6 +16,10 @@ async fn main() -> Result<(), Error> {
 }
 
 async fn hello(request: Request, context: Context) -> Result<impl IntoResponse, Error> {
+    if request.method() != Method::POST {
+        return Ok("Not allowed".to_string());
+    }
+
     Ok(format!(
         "hello {}",
         request
